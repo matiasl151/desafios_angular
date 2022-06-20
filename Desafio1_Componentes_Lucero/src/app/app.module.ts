8d3facec2e1939ba4703a1d638dccf06e030f43e
matiasl151 <matiaslucero9815@gmail.com>
2022-06-19 21:45:02 -0300

    'Added navbar, side and content component'

--- Desafio1_Componentes_Lucero/src/app/app.module.ts
+++ Desafio1_Componentes_Lucero/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LateralComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
