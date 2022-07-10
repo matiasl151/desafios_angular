bbd86036090c149949ae90795fdd05d626651ef5
Matias Lucero <matiaslucero9815@gmail.com>
2022-07-09 22:43:04 -0300

    added alumno interface

--- 1PF_Lucero/src/app/components/lista-alumnos/lista-alumnos.component.ts
+++ 1PF_Lucero/src/app/components/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/interfaces/alumno.interface';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-alumnos.component.scss']
 })
 export class ListaAlumnosComponent implements OnInit {
+  @Input() alumnos: Alumno[] = [
+    {
+      nombre: 'Juan',
+      apellido: 'Perez',
+      edad: 20,
+      curso: 'Angular'
+    },
+  ];
+
+
 
   constructor() { }
 
