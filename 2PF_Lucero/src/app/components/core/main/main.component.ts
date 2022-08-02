c63f7ed5c00c8766d9f049bc5e2bb5141cad901e
Matias Lucero <matiaslucero9815@gmail.com>
2022-08-02 20:33:22 -0300

    Final update desafio

--- 2PF_Lucero/src/app/components/core/main/main.component.ts
+++ 2PF_Lucero/src/app/components/core/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -9,13 +9,22 @@ import { Router } from '@angular/router';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  token = localStorage.getItem('token');
+  user = '';
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  ngOnInit() {
+    if (this.token == '123456789') {
+      this.user = 'admin';
+    } else {
+      this.user = 'user';
+    }
+  }
 
   constructor(
     private breakpointObserver: BreakpointObserver,
